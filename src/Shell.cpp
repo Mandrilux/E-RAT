40ca79214b0a8849c049e717a7f5ef3e2483407d
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2020-02-01 10:41:39 +0100

    rename project and finish shell

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -13,6 +13,8 @@ namespace ERat
 
 	bool	Shell::initialize()
 	{
+		_map.insert(std::make_pair("cmd", std::bind(&ERat::Shell::executeCommand, this, std::placeholders::_1)));
+		_map.insert(std::make_pair("list", std::bind(&ERat::Shell::listClients, this, std::placeholders::_1)));
 		return true;
 	}
 
@@ -23,9 +25,36 @@ namespace ERat
 		std::cout << "> ";
 		while (std::getline(std::cin, line))
 		{
-			std::cout << "Read: " << line.tokenize(" \t").front() << std::endl;
+			auto tokens = line.tokenize(" \t");
+			if (_map.find(tokens.front()) != _map.end()) {
+				_map[tokens.front()](tokens);
+			}
+			else if (!line.empty()) {
+				std::cout << "Command not found." << std::endl;
+			}
 			std::cout << "> ";
 		}
+	}
+
+	bool	Shell::executeCommand(std::list<cpp::String> const& args)
+	{
+		std::string commandLine;
+		auto	it = args.begin();
+
+		while (++it != args.end()) {
+			commandLine = commandLine + *it + " ";
+		}
+		if (commandLine.empty()) {
+			std::cout << "Missing command args" << std::endl;
+		}
+		else {
+			std::cout << "Calling execute command with: " << commandLine << std::endl;
+		}
+	}
+
+	bool	Shell::listClients(std::list<cpp::String> const&)
+	{
+		std::cout << "Connected clients list:" << std::endl;
 	}
 
 	Shell::~Shell() = default;
--- src/Shell.hpp
+++ src/Shell.hpp
@@ -1,17 +1,22 @@
 #include <map>
 #include <functional>
 #include <string>
+#include "String.hpp"
 
 namespace ERat
 {
 	class	Shell {
-		using callback_function = std::function<bool (std::string const&, std::vector<std::string> const&)>;
+		using callback_function = std::function<bool (std::list<cpp::String> const&)>;
 	public:
 		Shell();
 		~Shell();
 
 		bool	initialize();
 		void	run();
+
+	private:
+		bool	executeCommand(std::list<cpp::String> const& args);
+		bool	listClients(std::list<cpp::String> const& args);
 
 	private:
 		std::map<std::string, callback_function>	_map;

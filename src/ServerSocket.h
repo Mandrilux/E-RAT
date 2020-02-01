149ce6d276dd91ef802e1c13d1b5b6652aae512e
heraud_b <baptiste.heraud@epitech.eu>
2020-02-01 10:00:19 +0100

    Serveur

--- src/ServerSocket.h
+++ src/ServerSocket.h
@@ -1,3 +1,26 @@
+#ifndef SERVERSOCKET_H
+#define SERVERSOCKET_H
+
+#ifdef WIN32 /* si vous êtes sous Windows */
+
+#include <winsock2.h>
+
+#else /* si vous êtes sous Linux */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> /* close */
+#include <netdb.h> /* gethostbyname */
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1)
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+#endif
 
 class SocketServer
 {
@@ -8,3 +31,6 @@ class SocketServer
     protected:
         SOCKET mastersocket;
 };
+
+
+#endif //HTTP_SERVER_SOCKET_H
